5a9afb7d80d8eff60414b5359a1817ff56827f4e
Yvem Senna <t_jefersons@hapvida.com.br>
2023-02-25 16:30:20 -0300

    Registrando os Serviços na Classe Program

--- APICatalogo/APICatalogo/Program.cs
+++ APICatalogo/APICatalogo/Program.cs
@@ -1,3 +1,6 @@
+using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+
 namespace APICatalogo
 {
     public class Program
@@ -12,6 +15,9 @@ namespace APICatalogo
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<AppDbContext>(option => option.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
 
             var app = builder.Build();
 
