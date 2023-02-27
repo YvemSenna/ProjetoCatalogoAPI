2aa295f0ce87cad83ee0524534b4f27dff2a6a9f
Yvem Senna <t_jefersons@hapvida.com.br>
2023-02-26 21:49:55 -0300

    Criando Controladores

---
+++ APICatalogo/APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,17 @@
+﻿using APICatalogo.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ProdutosController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public ProdutosController(AppDbContext context)
+    {
+        _context = context;
+    }
+}
