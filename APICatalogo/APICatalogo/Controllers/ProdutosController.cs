b64b4be660ba170690273ce5e093f4845efc1010
Yvem Senna <t_jefersons@hapvida.com.br>
2023-02-26 22:33:42 -0300

    ProdutosController retornar id

--- APICatalogo/APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/APICatalogo/Controllers/ProdutosController.cs
@@ -1,4 +1,5 @@
 ﻿using APICatalogo.Context;
+using APICatalogo.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,4 +15,27 @@ public class ProdutosController : ControllerBase
     {
         _context = context;
     }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Produto>> Get()
+    {
+        var produtos = _context.Produtos.ToList();
+        if (produtos is null) 
+        {
+            return NotFound("Produtos não encontrados.");
+        }
+        return produtos;
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<Produto> Get(int id) 
+    {
+        var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+        if (produto is null) 
+        {
+            return NotFound("Produto não encontrado.");
+        }
+        return produto;
+    }
 }
+
