a4c5c43ad775d3d9868a8ed649629e69799cb9a1
Yvem Senna <t_jefersons@hapvida.com.br>
2023-02-27 00:21:48 -0300

    Ajustando a API : Serialização e Desserialização

--- APICatalogo/APICatalogo/Program.cs
+++ APICatalogo/APICatalogo/Program.cs
@@ -1,5 +1,6 @@
 using APICatalogo.Context;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 namespace APICatalogo;
 
@@ -11,7 +12,7 @@ public class Program
 
         // Add services to the container.
 
-        builder.Services.AddControllers();
+        builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
