91d6bf9a47fe451a21848df793ebc0a875959041
Yvem Senna <t_jefersons@hapvida.com.br>
2023-02-26 23:00:49 -0300

    ProdutosController - PUT : Ataualiza um Produto

--- APICatalogo/APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/APICatalogo/Controllers/ProdutosController.cs
@@ -2,6 +2,7 @@
 using APICatalogo.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace APICatalogo.Controllers;
 
@@ -48,6 +49,19 @@ public class ProdutosController : ControllerBase
         _context.SaveChanges();
 
         return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult Put(int id, Produto produto) 
+    {
+        if (id != produto.ProdutoId) 
+        {
+            return BadRequest();
+        }
+        _context.Entry(produto).State = EntityState.Modified;
+        _context.SaveChanges();
+
+        return Ok(produto);
     }
 }
 
